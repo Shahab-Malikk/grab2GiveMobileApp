bb22b0a1f93d460f3470921e60efbe5ca82f82da
Shahab Malik <shahabmalik227710@gmail.com>
2024-02-12 23:25:17 +0500

    Added Ui for showing ngos

---
+++ src/components/Ngos/AllNgos.jsx
@@ -0,0 +1,39 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import NgoCard from "./NgoCard";
+
+const AllNgos = () => {
+  const ngos = [
+    {
+      ngoName: "Eidhi ",
+      location: "Taxila",
+    },
+    {
+      nagoName: "The Hope",
+      location: "Rawalpindi",
+    },
+    {
+      nagoName: "SOS",
+      location: "Rawalpindi",
+    },
+  ];
+  return (
+    <View className="px-4">
+      <Text className="text-lg font-bold text-base800">Ngos</Text>
+      <Text className="text-lightGrey mt-2">
+        Here is list if ngos you can connect with.
+      </Text>
+      <View className="flex-1">
+        <ScrollView className="mt-4 ">
+          <View className="flex flex-row basis-1/2 flex-wrap justify-between">
+            {ngos.map((item) => (
+              <NgoCard ngoName={item.ngoName} location={item.location} />
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+
+export default AllNgos;
--- src/screens/NgosScreen.jsx
+++ src/screens/NgosScreen.jsx
@@ -1,11 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import AllNgos from "../components/Ngos/AllNgos";
 
 const NgosScreen = () => {
   return (
-    <View>
-      <Text>NgosScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 px-4 bg-bgLight">
+      <AllNgos />
+    </SafeAreaView>
   );
 };
 
